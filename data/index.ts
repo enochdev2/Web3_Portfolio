68d659ac5259d76ab808c22a247f82b7f1b20bb8
enoch-promise <enochpromise.va@gmail.com>
2024-06-05 04:22:21 -0700

    commit

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,8 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title:
+      "As a full-stack blockchain developer, I specialize in creating decentralized applications that redefine user interaction and financial transactions. With expertise in Solidity, Rust, Anchor (solana), React, Nextjs,  JavaScript, and a passion for DeFi, I deliver solutions that are secure, scalable, and aligned with the latest in blockchain innovation.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
