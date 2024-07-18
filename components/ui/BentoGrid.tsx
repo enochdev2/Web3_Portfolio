23e7bd0f47ed4a8fd7b50fade4951cdd0d52e95e
enochdev2 <enochthechristepistle@gmail.com>
2024-07-18 22:12:00 +0100

    implement navbar correctly

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -131,7 +131,7 @@ export const BentoGridItem = ({
           {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
           {/* remove mb-2 mt-2 */}
           <div
-            className={`${id == 1 ? "font-sans text-lg text-sky-900 lg:text-2xl max-w-full font-bold z-10" : "font-sans text-lg lg:text-3xl max-w-96 font-bold z-10"}`}
+            className={`${id == 1 ? "font-sans text-lg text-white lg:text-2xl max-w-full font-bold z-10" : "font-sans text-lg lg:text-3xl max-w-96 font-bold z-10"}`}
           >
             {title}
           </div>
@@ -141,7 +141,7 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 5 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-5">
               {/* tech stack lists */}
               <div className="flex flex-col gap-2 md:gap-2 lg:gap-4">
                 {leftLists.map((item, i) => (
@@ -160,7 +160,7 @@ export const BentoGridItem = ({
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-base lg:text-base opacity-50 
                     lg:opacity-100 rounded-lg text-center  bg-[#10132E]"
                   >
                     {item}
